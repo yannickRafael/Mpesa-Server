6b3ea1c55b18bcb8b84c4dfe32b0613c9624ecc3
yannickRafael <125164172+yannickRafael@users.noreply.github.com>
2023-05-13 20:08:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const querystring = require('querystring');
+const transaction = require('./mpesa.js')
+
+
+
+http.createServer(function (req, res) {
+    const query = querystring.parse(req.url.split('?')[1]); // Get the query parameters from the URL
+  
+    const amount = query.amount;
+    const phone = query.phone;
+    const ref = query.ref;
+    const thirdPartyRef = query.third_party_ref;
+  
+    if (amount && phone && ref && thirdPartyRef) {
+      transaction.c2b({
+        amount: amount,
+        msisdn: phone,
+        reference: ref,
+        third_party_reference: thirdPartyRef
+    })
+      // lide com o sucesso
+      .then(function(response){
+          console.log(response)
+          res.writeHead(200, response)
+          res.end
+      })
+      // lide com o erro
+      .catch(function(error){
+          console.log(error)
+      })
+    } else {
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write('Missing query parameters.');
+      res.end();
+    }
+  }).listen(8080);
---
+++ mpesa.js
@@ -0,0 +1,18 @@
+// inclua a biblioteca
+const Transaction = require('mpesa-mz-nodejs-lib')
+
+// crie o objeto de configuração
+const config = {
+    public_key: 'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAmptSWqV7cGUUJJhUBxsMLonux24u+FoTlrb+4Kgc6092JIszmI1QUoMohaDDXSVueXx6IXwYGsjjWY32HGXj1iQhkALXfObJ4DqXn5h6E8y5/xQYNAyd5bpN5Z8r892B6toGzZQVB7qtebH4apDjmvTi5FGZVjVYxalyyQkj4uQbbRQjgCkubSi45Xl4CGtLqZztsKssWz3mcKncgTnq3DHGYYEYiKq0xIj100LGbnvNz20Sgqmw/cH+Bua4GJsWYLEqf/h/yiMgiBbxFxsnwZl0im5vXDlwKPw+QnO2fscDhxZFAwV06bgG0oEoWm9FnjMsfvwm0rUNYFlZ+TOtCEhmhtFp+Tsx9jPCuOd5h2emGdSKD8A6jtwhNa7oQ8RtLEEqwAn44orENa1ibOkxMiiiFpmmJkwgZPOG/zMCjXIrrhDWTDUOZaPx/lEQoInJoE2i43VN/HTGCCw8dKQAwg0jsEXau5ixD0GUothqvuX3B9taoeoFAIvUPEq35YulprMM7ThdKodSHvhnwKG82dCsodRwY428kg2xM/UjiTENog4B6zzZfPhMxFlOSFX4MnrqkAS+8Jamhy1GgoHkEMrsT5+/ofjCx0HjKbT5NuA2V/lmzgJLl3jIERadLzuTYnKGWxVJcGLkWXlEPYLbiaKzbJb2sYxt+Kt5OxQqC1MCAwEAAQ==',
+    api_host: 'api.sandbox.vm.co.mz',
+    api_key: 'seenp11ug6n0a0pr50oa64ndltl8rpx5',
+    origin: '*',
+    service_provider_code: '171717',
+    initiator_identifier: 'MyLunch',
+    security_credential: '<Security Credential>'
+}
+
+// instancie o objeto Transaction, inicializando-o com a configuração válida
+const transaction = new Transaction(config)
+module.exports = transaction
+// inicie uma transação C2B baseada em promessa
