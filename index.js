9bd04e371c5f7de2ce7ce06c7759c1da07f20031
Yannick Rafael Matimbe <125164172+yannickRafael@users.noreply.github.com>
2023-05-30 07:10:52 +0200

    tratamento de excepcoes melhorada

--- index.js
+++ index.js
@@ -3,41 +3,37 @@ const querystring = require('querystring');
 const transaction = require('./mpesa.js');
 const { log } = require('console');
 
-console.log('iniciado')
+console.log('iniciado');
 
+http.createServer(async function (req, res) {
+  console.log('Servidor iniciou');
+  const query = querystring.parse(req.url.split('?')[1]); // Get the query parameters from the URL
 
+  const amount = query.amount;
+  const phone = query.phone;
+  const ref = query.ref;
+  const thirdPartyRef = query.third_party_ref;
 
-http.createServer(function (req, res) {
-
-    console.log('Servidor iniciou')
-    const query = querystring.parse(req.url.split('?')[1]); // Get the query parameters from the URL
-  
-    const amount = query.amount;
-    const phone = query.phone;
-    const ref = query.ref;
-    const thirdPartyRef = query.third_party_ref;
-  
-    if (amount && phone && ref && thirdPartyRef) {
-      
-      transaction.c2b({
+  if (amount && phone && ref && thirdPartyRef) {
+    try {
+      const response = await transaction.c2b({
         amount: amount,
         msisdn: phone,
         reference: ref,
         third_party_reference: thirdPartyRef
-    })
-      // lide com o sucesso
-      .then(function(response){
-          console.log(response)
-          res.writeHead(200, response)
-          res.end
-      })
-      // lide com o erro
-      .catch(function(error){
-          console.log(error)
-      })
-    } else {
-      res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.write('Missing query parameters.');
+      });
+      console.log(response);
+      res.writeHead(200, response);
+      res.end();
+    } catch (error) {
+      console.log(error);
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.write('An error occurred.');
       res.end();
     }
-  }).listen(8080);
+  } else {
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write('Missing query parameters.');
+    res.end();
+  }
+}).listen(8080);
