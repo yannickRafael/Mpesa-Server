ca759a37d86c21ab180e056c3998add5257d1b58
Yannick Rafael Matimbe <125164172+yannickRafael@users.noreply.github.com>
2023-06-16 20:43:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express');
+const http = require('http');
 const querystring = require('querystring');
 const transaction = require('./mpesa.js');
 const { log } = require('console');
 
 console.log('iniciado');
 
-const app = express();
-const port = 8080;
-
-app.get('/', async (req, res) => {
+http.createServer(async function (req, res) {
   console.log('Servidor iniciou');
-  const query = querystring.parse(req.url.split('?')[1]); // Obter os parâmetros da consulta da URL
+  const query = querystring.parse(req.url.split('?')[1]); // Get the query parameters from the URL
 
   const amount = query.amount;
   const phone = query.phone;
@@ -26,16 +23,17 @@ app.get('/', async (req, res) => {
         third_party_reference: thirdPartyRef
       });
       console.log(response);
-      res.status(200).send(response); // Enviar status de sucesso (200) e a resposta
+      res.writeHead(200, response);
+      res.end();
     } catch (error) {
       console.log(error);
-      res.status(500).send('An error occurred.'); // Enviar status de erro do servidor (500) e mensagem de erro
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.write('An error occurred.');
+      res.end();
     }
   } else {
-    res.status(400).send('Missing query parameters.'); // Enviar status de requisição inválida (400) e mensagem de erro
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write('Missing query parameters.');
+    res.end();
   }
-});
-
-app.listen(port, () => {
-  console.log(`Servidor iniciado na porta ${port}`);
-});
+}).listen(8080);
